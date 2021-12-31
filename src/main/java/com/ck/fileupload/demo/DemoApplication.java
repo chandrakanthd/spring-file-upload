abf0d47630620497dae4c213b325a562b16c027d
chandrakanth <chandrakanth@onnetsystems.net>
2021-12-31 12:41:40 +0530

    single file upload to local directory

---
+++ src/main/java/com/ck/fileupload/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.ck.fileupload.demo;
+
+import javax.annotation.Resource;
+
+import com.ck.fileupload.demo.service.FilesStorageService;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+  @Resource
+  FilesStorageService storageService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Override
+  public void run(String... arg) throws Exception {
+    storageService.deleteAll();
+    storageService.init();
+  }
+
+}
